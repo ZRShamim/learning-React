9553b17f231c9a2d9b73aa720f3653567f1d150a
ZRShamim <zrshamim8822@gmail.com>
2021-03-01 07:43:37 +0600

    Learning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { findByLabelText } from '@testing-library/react';
+import React, { useEffect, useState } from 'react';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 function App() {
   const products = [
@@ -15,13 +17,49 @@ function App() {
   // const productNames = products.map(product => product)
   return (
     <div className="App">
+      
       <header className="App-header">
+      <p>Learning React</p>
+      <Counter></Counter>
+      <Users></Users>
         {
         products.map(product => <Product product={product}></Product>)
         }
       </header>
     </div>
   );
+}
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+  // const handleIncrease = () => setCount(count + 1);
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  )
+}
+
+function Users(props) {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.email}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Product(props) {
